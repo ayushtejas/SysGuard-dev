16925cf8f2932ce53fc0393b930a40723366b70b
Ayush Kumar <ayushkumar@Ayushs-MacBook-Air.local>
2022-11-03 14:07:24 +0530

    changed STATICFILES_DIRS

--- graphical_pwd_auth/settings.py
+++ graphical_pwd_auth/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-STATIC_ROOT = os.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Password Grid NxN
 N = 6
